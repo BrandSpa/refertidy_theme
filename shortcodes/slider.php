394f4abba67cc4948318a0413d3b8644a7a13d4c
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-21 16:34:02 -0500

    get slides

--- shortcodes/slider.php
+++ shortcodes/slider.php
@@ -4,10 +4,12 @@ function parseSlides($slides) {
 	$parseAtts = function_exists('vc_param_group_parse_atts') ? vc_param_group_parse_atts( $slides ) : [];
 
 	$arrResult = array_map(function($slide) {
+		if(!empty($slide)) {
 			$slide['bg_img'] = wp_get_attachment_url($slide['bg_img']);			
 			$slide['model_img'] = wp_get_attachment_url($slide['model_img']);			
 			$slide['object_img'] = wp_get_attachment_url($slide['object_img']);			
 			return $slide;
+		}
 	}, $parseAtts);
 
 	return $arrResult;
@@ -16,8 +18,8 @@ function parseSlides($slides) {
 function ra_slider_sc( $atts , $content) {
   $at = shortcode_atts( [ 'slides' => '' ], $atts );
 		
-	//$slides = parseSlides($at['slides']); 
-	$slides = vc_param_group_parse_atts($atts['slides']); 
+	$slides = parseSlides($at['slides']); 
+	// $slides = vc_param_group_parse_atts($atts['slides']); 
 	$slidesJson = json_encode($slides);
 	
   ob_start();
