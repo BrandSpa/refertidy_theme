d3d4d5d05b020c300e1442d85792760622525346
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-21 17:57:08 -0500

    add height

--- header.php
+++ header.php
@@ -98,7 +98,7 @@
 	</div>
 </header>
 
-<form action="" style=" background: #6031BA; position: fixed; bottom: 0; z-index: 998">
+<form action="" style=" background: #6031BA; position: fixed; bottom: 0; z-index: 998; height: 100px">
 	<input type="text" placeholder="Nombre" />
 	<input type="text" placeholder="Email" />
 	<input type="text" placeholder="Télefono" />
