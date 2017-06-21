4a1880dfa9c5e24996e229698e6b93714ef1e976
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-21 16:35:12 -0500

    get vendor

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <?php wp_footer() ?>
 <!-- /wordpress files-->
 
+<script src="<?php echo get_template_directory_uri() ?>/client/dist/vendor.js"></script>
 <script src="<?php echo get_template_directory_uri() ?>/client/dist/client.js"></script>
-
 </body>
 </html>
