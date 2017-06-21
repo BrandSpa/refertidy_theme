dd711f1e700c08103347796e601ca288d4a548a6
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-21 17:37:55 -0500

    add bootstrap grid

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>RentAdvisor</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap-grid.css">
 	<style>
 		@keyframes slideModel {
 			from {
@@ -48,7 +49,9 @@
 <body>
 	
 <header id="app-header">
-	<img src="<?php echo get_template_directory_uri() ?>/imgs/logo.svg" alt="">
-	<ul></ul>
+	<div class="container">
+		<img src="<?php echo get_template_directory_uri() ?>/imgs/logo.svg" alt="">
+		<ul></ul>
+	</div>
 </header>
 
