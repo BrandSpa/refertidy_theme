01e38c04fddb4cf3cf8bcf9549e750a675bb2738
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-22 17:21:46 -0500

    add widget

--- widgets/header.php
+++ widgets/header.php
@@ -1,32 +1,50 @@
 <?php
 
-function register_foo_widget() {
-  register_widget( 'ra_Header_Widget' );
+// function register_foo_widget() {
+//   register_widget( 'ra_Header_Widget' );
+// }
+
+// add_action( 'widgets_init', 'register_foo_widget');
+
+// class ra_Header_Widget extends WP_Widget {
+
+// 	public function __construct() {
+// 		$widget_ops = array( 
+// 			'classname' => 'header_widget',
+// 			'description' => 'My Widget is awesome'
+// 		);
+
+// 		parent::__construct( 'header_widget', 'Header right Content', $widget_ops );
+// 	}
+
+// 	/**
+// 	 * Outputs the content of the widget
+// 	 *
+// 	 * @param array $args
+// 	 * @param array $instance
+// 	 */
+// 	public function widget( $args, $instance ) {
+// 		// outputs the content of the widget
+
+// 	}
+
+// }
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Home right sidebar',
+		'id'            => 'home_right_1',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
 }
 
-add_action( 'widgets_init', 'register_foo_widget');
-
-class ra_Header_Widget extends WP_Widget {
-
-	public function __construct() {
-		$widget_ops = array( 
-			'classname' => 'header_widget',
-			'description' => 'My Widget is awesome'
-		);
-
-		parent::__construct( 'header_widget', 'Header right Content', $widget_ops );
-	}
-
-	/**
-	 * Outputs the content of the widget
-	 *
-	 * @param array $args
-	 * @param array $instance
-	 */
-	public function widget( $args, $instance ) {
-		// outputs the content of the widget
-
-	}
-
-}
-
+add_action( 'widgets_init', 'arphabet_widgets_init' );
