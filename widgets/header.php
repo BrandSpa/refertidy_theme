bb19784a86330eb80fb0ba2a9e1e0851b959b083
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-22 16:09:25 -0500

    add methods

--- widgets/header.php
+++ widgets/header.php
@@ -2,14 +2,12 @@
 
 class Header_Widget extends WP_Widget {
 
-	/**
-	 * Sets up the widgets name etc
-	 */
 	public function __construct() {
 		$widget_ops = array( 
 			'classname' => 'header__widget',
 			'description' => 'header content'
 		);
+
 		parent::__construct( 'header_content', 'Header Content', $widget_ops );
 	}
 
@@ -23,6 +21,26 @@ class Header_Widget extends WP_Widget {
 		// outputs the content of the widget
 
 	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+	}
+
 }
 
 add_action('widgets_init', function() { 
