df5fecc43a3a48cd445c69f884f115fa231c6509
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-22 17:16:39 -0500

    change way of register

--- widgets/header.php
+++ widgets/header.php
@@ -1,14 +1,21 @@
 <?php
 
+function register_foo_widget() {
+  register_widget( 'ra_Header_Widget' );
+}
+
+add_action( 'widgets_init', 'register_foo_widget');
+
+
 class ra_Header_Widget extends WP_Widget {
 
 	public function __construct() {
 		$widget_ops = array( 
 			'classname' => 'header_widget',
-			'description' => 'My Widget is awesome',
+			'description' => 'My Widget is awesome'
 		);
 
-		parent::__construct( 'header_widget', 'Header Content', $widget_ops );
+		parent::__construct( 'header_widget', 'Header right Content', $widget_ops );
 	}
 
 	/**
@@ -22,32 +29,5 @@ class ra_Header_Widget extends WP_Widget {
 
 	}
 
-	/**
-	 * Outputs the options form on admin
-	 *
-	 * @param array $instance The widget options
-	 */
-	public function form( $instance ) {
-		// outputs the options form on admin
-
-	}
-
-	/**
-	 * Processing widget options on save
-	 *
-	 * @param array $new_instance The new options
-	 * @param array $old_instance The previous options
-	 */
-	public function update( $new_instance, $old_instance ) {
-		// processes widget options to be saved
-		
-	}
-
 }
 
-
-function register_foo_widget() {
-    register_widget( 'ra_Header_Widget' );
-}
-
-add_action( 'widgets_init', 'register_foo_widget');
