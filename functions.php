42ec0fa3b034e8f97e8c719b65d078fa8d4d6bb6
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-22 14:42:45 -0500

    header nav

--- functions.php
+++ functions.php
@@ -4,6 +4,11 @@
 include_once 'shortcodes/slider.php';
 //VISUALCOMPOSER
 include_once 'shortcodes/vc/slider.php';
-
 //LIBS
 include_once 'lib/clean_menu.php';
+//MENUS
+register_nav_menus(
+  array(
+    'header' => 'Header Nav'
+  )
+);
