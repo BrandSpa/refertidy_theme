152bf7a23f9cb6124726b8681c171adfb5f1e114
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-22 16:52:44 -0500

    register header widget

--- functions.php
+++ functions.php
@@ -7,7 +7,55 @@ include_once 'shortcodes/vc/slider.php';
 //LIBS
 include_once 'lib/clean_menu.php';
 //WIDGETS
-include_once 'widgets/header.php';
+class Header_Widget extends WP_Widget {
+
+	public function __construct() {
+		$widget_ops = array( 
+			'classname' => 'header_widget',
+			'description' => 'header content'
+		);
+
+		parent::__construct( 'header_widget', 'Header Content', $widget_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		// outputs the content of the widget
+
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+		
+	}
+
+}
+
+add_action('widgets_init', function() {
+	register_widget( 'header_widget' );
+});
+
 
 //MENUS
 register_nav_menus(
