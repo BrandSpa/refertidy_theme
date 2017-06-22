e62351e2453a0a077c6c9f44ad3747e652d405b0
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-22 15:55:56 -0500

    register header widget

--- functions.php
+++ functions.php
@@ -2,10 +2,12 @@
 
 //SHORTCODES
 include_once 'shortcodes/slider.php';
-//VISUALCOMPOSER
+//VISUAL COMPOSER
 include_once 'shortcodes/vc/slider.php';
 //LIBS
 include_once 'lib/clean_menu.php';
+//WIDGETS
+include_once 'widgets/header.php';
 //MENUS
 register_nav_menus(
   array(
---
+++ widgets/header.php
@@ -0,0 +1,40 @@
+<?php
+
+class Header_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		$widget_ops = array( 
+			'classname' => 'header__widget'
+		);
+		parent::__construct( 'Header_widget', 'Header widget', $widget_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		// outputs the content of the widget
+
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+
+	}
+}
+
+add_action('widgets_init', function() { 
+	register_widget( 'Header_Widget' );
+});
