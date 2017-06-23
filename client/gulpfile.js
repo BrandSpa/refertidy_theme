c2297347dd8abb1248701d50beeb0e082453ef2c
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-23 15:50:35 -0500

    sort sass

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,9 +1,14 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
     gulp.src('sass/index.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
         .pipe(gulp.dest('./dist/'))
 });
 
