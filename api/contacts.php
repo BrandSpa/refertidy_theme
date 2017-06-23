55ac6e04df8cfec8ffd92407a8c64debb125e208
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-23 12:05:49 -0500

    add action

--- api/contacts.php
+++ api/contacts.php
@@ -2,6 +2,7 @@
 require_once get_template_directory() . '/lib/response_json.php';
 
 add_action( 'wp_ajax_nopriv_store_contact', 'store_contact' );
+add_action( 'wp_ajax_store_contact', 'store_contact' );
 
 function store_contact() {
 	global $wpdb;
@@ -9,7 +10,7 @@ function store_contact() {
 		'name' => $_POST['name'],
 		'email' => $_POST['email'],
 		'phone' => $_POST['phone'],
-		'product' => $_POST['product'],
+		'product' => $_POST['product']
 	];
 
 	$res = $wpdb->insert( 'contacts', 
