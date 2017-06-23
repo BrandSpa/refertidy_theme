099c82eaa576dac519b3f9eea6f54d55940c1821
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-23 16:46:20 -0500

    check params

--- shortcodes/slider.php
+++ shortcodes/slider.php
@@ -2,17 +2,21 @@
 $dir_base = get_template_directory();
 require_once $dir_base . '/vendor/autoload.php';
 
+function checkParam($param) {
+	return isset($param) ? $param : '';
+}
+
 function parseSlides($slides) {
 	$parseAtts = function_exists('vc_param_group_parse_atts') ? vc_param_group_parse_atts( $slides ) : [];
 
 	$arrResult = array_map(function($slide) {
 		if(!empty($slide)) {
-			$slide['bg_img'] = wp_get_attachment_url($slide['bg_img']);			
-			$slide['bg_img_mobile'] = wp_get_attachment_url($slide['bg_img_mobile']);			
-			$slide['model_img'] = wp_get_attachment_url($slide['model_img']);			
-			$slide['model_img_mobile'] = wp_get_attachment_url($slide['model_img_mobile']);			
-			$slide['object_img'] = wp_get_attachment_url($slide['object_img']);			
-			$slide['object_img_mobile'] = wp_get_attachment_url($slide['object_img_mobile']);			
+			$slide['bg_img'] = wp_get_attachment_url(checkParam($slide['bg_img']));			
+			$slide['bg_img_mobile'] = wp_get_attachment_url(checkParam($slide['bg_img_mobile']));			
+			$slide['model_img'] = wp_get_attachment_url(checkParam($slide['model_img']));
+			$slide['model_img_mobile'] = wp_get_attachment_url(checkParam($slide['model_img_mobile']));			
+			$slide['object_img'] = wp_get_attachment_url(checkParam($slide['object_img']));
+			$slide['object_img_mobile'] = wp_get_attachment_url(checkParam($slide['object_img_mobile']));			
 		}
 		
 		return $slide;
@@ -32,9 +36,12 @@ function ra_slider_sc( $atts , $content) {
 
   ob_start();
 	?>
+
 	<!--ra_slider html-->
 	<?php if(!$detect->isMobile()): ?>
 		<div class="ra-slider" data-props='{"slides": <?php echo $slidesJson ?>}'></div>
+		<?php else: ?>
+		<div class="ra-slider-mobile" data-props='{"slides": <?php echo $slidesJson ?>}'></div>
 	<?php endif ?>
 	<!--/ra_slider html-->
 	<?php
