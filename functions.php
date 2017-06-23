fb64afea3e2ae15e32a68fd179e80c33e129f8df
Alejandro Sanabria <alesanabriav@gmail.com>
2017-06-23 12:07:54 -0500

    include api endpoint

--- functions.php
+++ functions.php
@@ -8,7 +8,8 @@ include_once 'shortcodes/vc/slider.php';
 //LIBS
 include_once 'lib/clean_menu.php';
 include_once 'lib/create_table.php';
-
+//API 
+include_once 'api/contacts.php';
 //MENUS
 register_nav_menus(
   array(
