608e8e70604343d0efa0379970cb462b3e3bb2f7
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-04 15:01:48 -0500

    validate bool

--- api/contacts.php
+++ api/contacts.php
@@ -17,7 +17,7 @@ function store_contact() {
 		'name' => $data['name'],
 		'email' => $data['email'],
 		'question' => $data['question'],
-		'privacy' => $data['privacy'],
+		'privacy' => isset($data['privacy']) ? $data['privacy'] : 'false',
 		'phone' => isset($data['phone']) ? $data['phone'] : '',
 		'company' => isset($data['company']) ? $data['company'] : ''
 	];
