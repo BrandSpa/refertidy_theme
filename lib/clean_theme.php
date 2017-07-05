1dafd7831b64baa521fcbe2b1fb86f6366470df1
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 15:54:43 -0500

    left jquery

--- lib/clean_theme.php
+++ lib/clean_theme.php
@@ -1,12 +1,12 @@
 <?php
 
-function modify_jquery() {
-	if (!is_admin()) {
-		wp_deregister_script( 'wp-embed' );
-		wp_deregister_script('jquery');
-		wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', false, '3.1.1', true);
-		wp_enqueue_script('jquery');
-	}
-}
-
-add_action('init', 'modify_jquery');
+// function modify_jquery() {
+// 	if (!is_admin()) {
+// 		wp_deregister_script( 'wp-embed' );
+// 		wp_deregister_script('jquery');
+// 		wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', false, '3.1.1', true);
+// 		wp_enqueue_script('jquery');
+// 	}
+// }
+//
+// add_action('init', 'modify_jquery');
