bccde75b3d5264563cadab7f7bd4841ecc0c6d0e
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 18:42:09 -0500

    enqueue later

--- lib/clean_theme.php
+++ lib/clean_theme.php
@@ -4,14 +4,9 @@ function modify_jquery() {
 	if (!is_admin()) {
     $wp_scripts = wp_scripts();
 
-		wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', false, '3.1.1', true);
-
     foreach ($wp_scripts->queue as $name) {
       wp_deregister_script($name);
     }
-
-    wp_enqueue_script('jquery');
-
 
 	}
 }
