a4e86aa00fb56531d2b75cd38076ad47a1c0cb2a
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 18:10:08 -0500

    simple script

--- lib/combine_js.php
+++ lib/combine_js.php
@@ -1,13 +1,7 @@
 <?php
-header("content-type: text/javascript; charset: UTF-8");
 
-function combine_js() {
+$wp_scripts = wp_scripts();
 
-  $wp_scripts = wp_scripts();
-  foreach ($wp_scripts->queue as $script) {
-    echo ABSPATH . $script->src;
-  }
-
+foreach ($wp_scripts->queue as $script) {
+  echo ABSPATH . $script->src;
 }
-
-combine_js();
