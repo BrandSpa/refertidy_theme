886388618ac465ec7db353e28062626e0b32b7da
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 15:05:22 -0500

    add gotham rounder

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 	<title>RentAdvisor</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap-grid.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/fonts/aktiv.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/fonts/gotham_rounded.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/client/dist/index.css?v=<?php echo filemtime(get_template_directory() . '/client/dist/index.css') ?>">
 
 	<!--wordpress files-->
