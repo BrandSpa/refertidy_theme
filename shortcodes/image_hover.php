40d8b5fa5875343b421b3f8bad4a42883f54a3c4
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 14:31:06 -0500

    add image hover css

--- shortcodes/image_hover.php
+++ shortcodes/image_hover.php
@@ -7,11 +7,15 @@ function ra_image_hover_sc( $atts ) {
     "image_overlay" => "",
     "bg_color"  => "#000"
   ], $atts);
+
+	$at['image'] = wp_get_attachment_url($at['image_overlay']);
+	$at['image_overlay'] = wp_get_attachment_url($at['image_overlay']);
+
 	ob_start();
 	?>
 
 		<!--ra_image_hover-->
-    <div class="img_hover" style="background-color:<?php echo $at['bg_color'] ?>">
+    <div class="img_hover">
       <img data-src="<?php echo $at['image_overlay'] ?>" alt="" class="img_hover__overlay lazyload" />
       <img data-src="<?php echo $at['image'] ?>" alt="" class="img_hover__base lazyload" />
     </div>
