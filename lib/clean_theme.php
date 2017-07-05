5960dfdd382b1ef1023a89e6c204fef52824f514
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 18:41:09 -0500

    enqueue later

--- lib/clean_theme.php
+++ lib/clean_theme.php
@@ -5,12 +5,12 @@ function modify_jquery() {
     $wp_scripts = wp_scripts();
 
 		wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', false, '3.1.1', true);
-		wp_enqueue_script('jquery');
-
 
     foreach ($wp_scripts->queue as $name) {
       wp_deregister_script($name);
     }
+
+    wp_enqueue_script('jquery');
 
 
 	}
