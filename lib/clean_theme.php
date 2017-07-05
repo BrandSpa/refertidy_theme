a4b00529c333d77902a4195ed294710ff9fd1934
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 18:40:15 -0500

    deregister

--- lib/clean_theme.php
+++ lib/clean_theme.php
@@ -4,12 +4,14 @@ function modify_jquery() {
 	if (!is_admin()) {
     $wp_scripts = wp_scripts();
 
+		wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', false, '3.1.1', true);
+		wp_enqueue_script('jquery');
+
+
     foreach ($wp_scripts->queue as $name) {
       wp_deregister_script($name);
     }
 
-		wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', false, '3.1.1', true);
-		wp_enqueue_script('jquery');
 
 	}
 }
