60cc83cc5cabe37fd28656daa676bf6cd705c89c
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 17:52:00 -0500

    deregister scripts

--- lib/clean_theme.php
+++ lib/clean_theme.php
@@ -6,6 +6,12 @@ function modify_jquery() {
 		wp_deregister_script('jquery');
 		wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', false, '3.1.1', true);
 		wp_enqueue_script('jquery');
+    $wp_scripts = wp_scripts();
+    foreach ($wp_scripts->queue as $name) {
+      wp_deregister_script($name);
+      $wp_scripts->registered[$name]->src;
+    }
+
 	}
 }
 
