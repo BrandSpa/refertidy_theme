812d2301e204a8d719a93b43baaa086977a82c31
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-05 17:12:39 -0500

    async load appjs

--- footer.php
+++ footer.php
@@ -2,8 +2,42 @@
 <?php wp_footer() ?>
 <!-- /wordpress files-->
 
-<script src="<?php echo get_template_directory_uri() ?>/client/dist/vendor.js"></script>
-<script src="<?php echo get_template_directory_uri() ?>/client/dist/client.js?v=<?php echo filemtime(get_template_directory() . '/client/dist/client.js') ?>"></script>
+<!--async load app-->
+<script type="text/javascript">
+function appendScript(src) {
+  var element = document.createElement("script");
+  element.src = src;
+  document.body.appendChild(element);
+}
+
+function appendLink(href) {
+  var element = document.createElement("link");
+  element.rel = 'stylesheet';
+  element.href = href;
+  element.type = 'text/css';
+  element.media = 'all';
+  document.head.appendChild(element);
+}
+
+function downloadJS (){
+  [
+    "<?php echo get_template_directory_uri() ?>/client/dist/vendor.js",
+    "<?php echo get_template_directory_uri() ?>/client/dist/client.js?v=<?php echo filemtime(get_template_directory() . '/client/dist/client.js') ?>"
+  ]
+  .forEach(function(src) {
+    appendScript(src);
+  });
+}
+
+if (window.addEventListener) {
+  window.addEventListener("load", downloadJS, false);
+} else if (window.attachEvent) {
+  window.attachEvent("onload", downloadJS);
+} else {
+  window.onload = downloadJS;
+}
+</script>
+<!--/async load app-->
 
 </body>
 </html>
