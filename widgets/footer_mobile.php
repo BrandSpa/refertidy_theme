8f74b94c9367063a445545cbdfe42dd9185d7573
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-06 09:36:28 -0500

    add widget

--- widgets/footer_mobile.php
+++ widgets/footer_mobile.php
@@ -3,21 +3,12 @@
 function ra_widgets_init() {
 
 	register_sidebar( array(
-		'name'          => 'Header right',
+		'name'          => 'Header right sidebar',
 		'id'            => 'header_widget',
 		'before_widget' => '<div>',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h2 class="rounded">',
 		'after_title'   => '</h2>',
-	) );
-
-	register_sidebar( array(
-		'name'          => 'Footer fixed mobile',
-		'id'            => 'footer_fixed_widget',
-		'before_widget' => '<div>',
-		'after_widget'  => '</div>',
-		'before_title'  => '',
-		'after_title'   => '',
 	) );
 
 }
