f2471bd2bebbace42226ee3b0d9a6ab67e92beb1
Alejandro Sanabria <alesanabriav@gmail.com>
2017-07-26 10:29:41 -0500

    change margin

--- templates/logo.php
+++ templates/logo.php
@@ -1,5 +1,5 @@
 <svg class="logo" width="200px" height="60px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 891.6 234" style="enable-background:new 0 0 891.6 234;" xml:space="preserve">
+	 viewBox="0 0 891.6 234" style="margin: -10px 0 0 0" xml:space="preserve">
 <style type="text/css">
 	.st0{fill:#602ABC;}
 </style>
