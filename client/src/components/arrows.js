c0d41f5734c57ba3522b69087d00134d2e1bc261
michael <michaelsanchez@brandspa.com>
2018-08-23 17:21:14 -0500

    arrows

--- client/src/components/arrows.js
+++ client/src/components/arrows.js
@@ -2,30 +2,12 @@ import React from 'react';
 
 export const ArrowLeft = () => {
   return (
-    <svg width="24px" height="44px" viewBox="0 0 24 44" version="1.1">
-        <defs></defs>
-        <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
-            <g id="Desktop-Home-Copy-5" transform="translate(-48.000000, -408.000000)" stroke="#ffffff" strokeWidth="4">
-                <g id="Group-17" transform="translate(50.000000, 410.000000)">
-                    <polyline id="Path-2" transform="translate(10.000000, 20.000000) rotate(90.000000) translate(-10.000000, -20.000000) " points="11.2,12 19.1,22.5 11.1,32"></polyline>
-                </g>
-            </g>
-        </g>
-    </svg>
+    <svg width="14px" height="34px" viewBox="0 0 24 44" version="1.1"><defs></defs><g id="Page-1" stroke="none" stroke-width="6" fill="none" fill-rule="evenodd" stroke-linecap="bevel" stroke-linejoin="round"><g id="Desktop-Home-Copy-5" transform="translate(-48.000000, -408.000000)" stroke="#fff" stroke-width="8"><g id="Group-17" transform="translate(50.000000, 410.000000)"><polyline id="Path-2" transform="translate(10.000000, 20.000000) rotate(90.000000) translate(-10.000000, -20.000000) " points="-10 10 10.261839 30 30 10.5169106"></polyline></g></g></g></svg>
   )
 }
 
 export const ArrowRight = () => {
   return (
-    <svg width="24px" height="44px" viewBox="0 0 24 44" version="1.1">
-        <defs></defs>
-			<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-			<g id="Desktop-Home-Copy-5" transform="translate(-1344.000000, -408.000000)" stroke="#fff" stroke-width="4">
-				<g id="Group-17" transform="translate(50.000000, 410.000000)">
-					<polyline id="Path-2-Copy" transform="translate(1306.000000, 20.000000) scale(-1, 1) rotate(90.000000) translate(-1306.000000, -20.000000) " points="14.1,12.1 6.1,22.6 14.2,32.1"></polyline>
-				</g>
-			</g>
-		</g>
-    </svg>
+    <svg width="14px" height="34px" viewBox="0 0 24 44" version="1.1"><defs></defs><g id="Page-1" stroke="none" stroke-width="6" fill="none" fill-rule="evenodd" stroke-linecap="bevel" stroke-linejoin="round"><g id="Desktop-Home-Copy-5" transform="translate(-1344.000000, -408.000000)" stroke="#fff" stroke-width="8"><g id="Group-17" transform="translate(50.000000, 410.000000)"><polyline id="Path-2-Copy" transform="translate(1306.000000, 20.000000) scale(-1, 1) rotate(90.000000) translate(-1306.000000, -20.000000) " points="1286 10 1306.26184 26.1 1326 10.5169106"></polyline></g></g></g></svg>
   )
 }
